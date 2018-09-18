87b5110db1a82b67b9021c19e528011d6f31ffad
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-18 11:10:13 +0300

    Add Unit Test with Test Doublers.

--- src/test/java/com/acme/edu/ArrayListTest.java
+++ src/test/java/com/acme/edu/ArrayListTest.java
@@ -3,6 +3,8 @@ package com.acme.edu;
 import org.hamcrest.CoreMatchers.*;
 import org.fest.assertions.api.Assertions;
 import org.junit.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class ArrayListTest {
     private ArrayList<Object> sut;
@@ -83,4 +86,59 @@ public class ArrayListTest {
             .contains("test 1");
         //endregion
     }
+
+    /**
+     * State-based testing
+     */
+    @Test //Counter anti-pattern
+    public void shouldGetStringRepresentationFromElementsWhenToString2() {
+        //region Given
+        Object stub1 = mock(Object.class);
+        when(stub1.toString()).thenReturn("test 1"); //PowerMockito
+
+        Object stub2 = mock(Object.class);
+        when(stub2.toString()).thenAnswer(ctx -> {
+            return "2";
+        });
+
+        sut.add(stub1);
+        sut.add(stub2);
+        //endregion
+
+        Assertions.assertThat(sut.toString())
+                .contains("2")
+                .contains("test 1");
+
+
+    }
+
+    /**
+     * Interaction-based testing
+     */
+    @Test @Ignore //Counter anti-pattern
+    public void shouldGetStringRepresentationFromElementsWhenToString3() {
+        //region Given
+        Object mock1 = mock(Object.class);
+        Object mock2 = mock(Object.class);
+
+        sut.add(mock1);
+        sut.add(mock2);
+        //endregion
+
+        //region When
+        sut.toString();
+        //endregion
+
+        //region Then
+//        verify(mock1).toString(any(MyClass.class));
+        verify(mock1).toString();
+        verify(mock2, times(1)).toString();
+        //endregion
+    }
+
+    //region Demo
+    private static<T> T myMock(Class<T> typeInfo) {
+        return null;
+    }
+    //endregion
 }
